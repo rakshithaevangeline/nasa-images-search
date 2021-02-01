07f4bfa3726534ba880fef5383ca999d59dedab6
Rakshitha Evangeline <rakshithaevangeline29@gmail.com>
2021-02-01 16:18:44 +0530

    Initial implementation

---
+++ main.js
@@ -0,0 +1,42 @@
+let axios = require("axios");
+
+const getDataForQuery = (query) => {
+  let config = {
+    params: {
+      q: query,
+    },
+  };
+
+  // Get the entire collection for the query
+  axios
+    .get("https://images-api.nasa.gov/search", config)
+    .then((res) => {
+      let fullCollection = res.data;
+
+      // Get the first three items in the collection
+      let firstThreeItems = fullCollection.collection.items.slice(0, 3);
+      console.log(firstThreeItems);
+
+      // Get a link for each item's thumbnail
+      firstThreeItems.forEach((item) => {
+        console.log(item.links[0].href);
+      });
+
+      // Get the description for each item
+      firstThreeItems.forEach((item) => {
+        console.log(item.data[0].description);
+      });
+    })
+    .catch((e) => {
+      console.log(e);
+    });
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+  let button = document.querySelector("button");
+  let textInput = document.querySelector("#text-input");
+
+  button.addEventListener("click", () => {
+    getDataForQuery(textInput.value);
+  });
+});
